b533ed6c0514065361c49194847cce9dc1465413
Shivang <shivangtips@gmail.com>
2026-03-22 15:32:14 +0530

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Providers } from "./providers"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
